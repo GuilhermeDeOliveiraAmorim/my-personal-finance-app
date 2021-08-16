8b4c5c830dfd03f4107d063afeef5660ef3c0f47
Guilherme Amorim <guilherme.o.a.ufal@gmail.com>
2021-08-16 18:44:59 -0300

    Saved Project

---
+++ src/main/java/com/guilhermeamorim/security/MyPersonalFinanceAppApplication.java
@@ -0,0 +1,13 @@
+package com.guilhermeamorim.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyPersonalFinanceAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyPersonalFinanceAppApplication.class, args);
+	}
+
+}
